c96394f20bde05fa5d36481d7bab88a9bd0792e6
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-11-08 23:36:41 -0500

    clean up files

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -5,12 +5,11 @@ import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 // idb:
 import { idbPromise } from "../../utils/helpers";
 // redux: 
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 const CartItem = ({ item }) => {
   // const [, dispatch] = useStoreContext();
 
-  const state = useSelector(state => state);
   const dispatch = useDispatch();
 
   const removeFromCart = item => {
